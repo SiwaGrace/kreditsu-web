30b76d52c75ff4a4457a53c2b02f7a1a451fa8c3
SiwaGrace <moneyesenam@gmail.com>
2026-03-09 20:22:43 +0000

    nothing really

--- src/pages/sme/BusinessProfilePage.jsx
+++ src/pages/sme/BusinessProfilePage.jsx
@@ -4,6 +4,11 @@ const BusinessProfilePage = () => {
   return (
     <div>
       <h1>Business Profile Page</h1>
+      <p>
+        My Business page = edit business name, description, industry upload logo
+        update contact info manage visibility (publish/unpublish) view their
+        public profile link
+      </p>
     </div>
   );
 };
